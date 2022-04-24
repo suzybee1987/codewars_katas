4428d90edd7f7d1e3ba4e77ef922924e132c3822
suzybee1987 <suzannemiller1987@gmail.com>
2022-04-24 17:43:14 +0100

    add codewars katas

---
+++ 8kyu_howGoodAreYouReally.js
@@ -0,0 +1,18 @@
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+    function betterThanAverage(classPoints, yourPoints) {
+        let result = classPoints.reduce((a, b) => a + b)
+        let average = (result + yourPoints) / classPoints.length;
+        return (average > yourPoints) ? false : true;
+    };
+
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9))
+console.log(betterThanAverage([2, 3], 5))
